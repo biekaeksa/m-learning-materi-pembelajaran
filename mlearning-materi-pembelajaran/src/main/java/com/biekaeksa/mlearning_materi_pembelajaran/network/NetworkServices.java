09880f8f910e398957634842313a32a405605912
biekaeksa <biekaeksabiek@gmail.com>
2018-05-18 14:33:27 +0700

    fix error class NetworkServices

--- mlearning-materi-pembelajaran/src/main/java/com/biekaeksa/mlearning_materi_pembelajaran/network/NetworkServices.java
+++ mlearning-materi-pembelajaran/src/main/java/com/biekaeksa/mlearning_materi_pembelajaran/network/NetworkServices.java
@@ -1,8 +1,11 @@
 package com.biekaeksa.mlearning_materi_pembelajaran.network;
 
+import org.reactivestreams.Subscription;
+
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Flowable;
+import io.reactivex.FlowableSubscriber;
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -18,63 +21,66 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NetworkServices {
-    public static class Builder {
-        private String url;
-        private Observable<Object> observable;
+    public Retrofit urlNetwork(String url) {
+        OkHttpClient.Builder builder = new OkHttpClient().newBuilder();
+        builder.readTimeout(30, TimeUnit.SECONDS);
+        builder.connectTimeout(15, TimeUnit.SECONDS);
+        builder.writeTimeout(15, TimeUnit.SECONDS);
 
-        public Builder(String url, Observable<Object> observable) {
-            this.url = url;
-            this.observable = observable;
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(interceptor).build();
+
+
+        return new Retrofit.Builder()
+                .baseUrl(url)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build();
+    }
+
+    public static class Builder<T> {
+        private Flowable<T> flowable;
+
+        public Builder(Flowable<T> flowable) {
+            this.flowable = flowable;
         }
 
         public Builder() {
         }
 
-        private Retrofit create(String url) {
-            OkHttpClient.Builder builder = new OkHttpClient().newBuilder();
-            builder.readTimeout(30, TimeUnit.SECONDS);
-            builder.connectTimeout(15, TimeUnit.SECONDS);
-            builder.writeTimeout(15, TimeUnit.SECONDS);
 
-            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-            OkHttpClient client = new OkHttpClient.Builder()
-                    .addInterceptor(interceptor).build();
-
-
-            return new Retrofit.Builder()
-                    .baseUrl(url)
-                    .client(client)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                    .build();
-        }
-
-
-        private interface CallBack {
-            void onSuccess(Object response);
+        private interface CallBack<T> {
+            void onSuccess(T response);
 
             void onError(Throwable e);
         }
 
 
-        public Builder observables(final Observable<Object> observable) {
-            this.observable = observable;
+        public Builder setFlowables(final Flowable<T> flowable) {
+            this.flowable = flowable;
             return this;
         }
 
-        public void load(final CallBack callBack) {
-            observable.subscribeOn(Schedulers.io())
+        public void load(final CallBack<T> callBack) {
+            flowable.subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(new DisposableObserver<Object>() {
+                    .subscribe(new FlowableSubscriber<T>() {
                         @Override
-                        public void onNext(Object o) {
-                            callBack.onSuccess(o);
+                        public void onSubscribe(Subscription s) {
+
                         }
 
                         @Override
-                        public void onError(Throwable e) {
-                            callBack.onError(e);
+                        public void onNext(T t) {
+                            callBack.onSuccess(t);
+                        }
+
+                        @Override
+                        public void onError(Throwable t) {
+                            callBack.onError(t);
                         }
 
                         @Override
