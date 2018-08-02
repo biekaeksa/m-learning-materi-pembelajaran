4fe204d59dde9dc4c0ec8ce441f2ba1ba584ef57
Biekaeksa <biekaeksabiek@gmail.com>
2018-08-02 18:04:19 +0700

    Update fix

--- mlearning-materi-pembelajaran/src/main/java/com/biekaeksa/mlearning_materi_pembelajaran/video/VideoLoader.java
+++ mlearning-materi-pembelajaran/src/main/java/com/biekaeksa/mlearning_materi_pembelajaran/video/VideoLoader.java
@@ -1,12 +1,19 @@
 package com.biekaeksa.mlearning_materi_pembelajaran.video;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.VideoView;
 
 import com.pierfrancescosoffritti.youtubeplayer.player.AbstractYouTubePlayerListener;
@@ -28,7 +35,7 @@ public class VideoLoader {
     }
 
 
-    public void playVideo(String urlVideo, VideoView videoView){
+    public void playVideo(String urlVideo, VideoView videoView) {
         Uri uriVideo = Uri.parse(urlVideo);
         videoView.setVideoURI(uriVideo);
         FullScreenMediaController vidControl = new FullScreenMediaController(activity);
@@ -36,10 +43,14 @@ public class VideoLoader {
         vidControl.setAnchorView(videoView);
         videoView.setMediaController(vidControl);
         videoView.start();
-//        videoView.setOnPreparedListener(mp -> );
+
+        ProgressBarHandler progressBarHandler = new ProgressBarHandler(activity);
+        progressBarHandler.show();
+
+        videoView.setOnPreparedListener(mp -> progressBarHandler.hide());
     }
 
-    public void playYoutubeVideo(YouTubePlayerView youTubePlayer, String idVideo){
+    public void playYoutubeVideo(YouTubePlayerView youTubePlayer, String idVideo) {
         youTubePlayer.initialize(youTubePlayer1 -> youTubePlayer1.addListener(new AbstractYouTubePlayerListener() {
             @Override
             public void onReady() {
@@ -49,6 +60,39 @@ public class VideoLoader {
 
     }
 
+    private class ProgressBarHandler {
+        private ProgressBar mProgressBar;
+        private Context mContext;
+
+        ProgressBarHandler(Context context) {
+            mContext = context;
+
+            ViewGroup layout = (ViewGroup) ((Activity) context).findViewById(android.R.id.content).getRootView();
+
+            mProgressBar = new ProgressBar(context, null, android.R.attr.progressBarStyleLarge);
+            mProgressBar.setIndeterminate(true);
+
+            RelativeLayout.LayoutParams params = new
+                    RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+            params.addRule(RelativeLayout.CENTER_IN_PARENT);
+            RelativeLayout rl = new RelativeLayout(context);
+
+            rl.setGravity(Gravity.CENTER);
+            rl.addView(mProgressBar);
+
+            layout.addView(rl, params);
+
+            hide();
+        }
+
+        void show() {
+            mProgressBar.setVisibility(View.VISIBLE);
+        }
+
+        void hide() {
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
 
 
 }
